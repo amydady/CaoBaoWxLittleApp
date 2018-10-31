02d36b3a0b2bd4a62d04add2f8ec556ed98fdac4
cheney008 <356421996@163.com>
2018-10-31 21:52:36 +0800

    little app

--- pages/groupOwner/groupOwner.js
+++ pages/groupOwner/groupOwner.js
@@ -1,17 +1,18 @@
-// pages/welfare/welfare.js
+var app = getApp();
 Page({
 
        /**
         * 页面的初始数据
         */
        data: {
-              RecordList: [{
-                     "time": '2018.10.12',
-                     "price": '100'
-              }, {
-                     "time": '2018.10.13',
-                     "price": '400'
-              }]
+              addressInfo: {},
+              isModify: false,
+              hasAddress: false,
+              shopName:'',
+              name:'',
+              mobile: '',
+              idCardFront: '',
+              idCardBack: '',
        },
 
        /**
@@ -21,11 +22,63 @@ Page({
 
        },
 
+       /**
+        * 生命周期函数--监听页面加载
+        */
+       onShow: function(options) {
+              var self = this;
+              wx.request({
+                     url: app.globalData.serverUrl + "/rest/littlecat/caobao/tuan/getById?id=" + app.globalData.openID, //给函数传递服务器地址参数
+                     data: {
+
+                     }, //给服务器传递数据，本次请求不需要数据，可以不填
+                     method: "GET",
+                     header: {
+                            'content-type': 'application/json' // 默认值，返回的数据设置为json数组格式
+                     },
+                     success: function(res) {
+                            console.log('groupOwner', res);
+                 
+                            if (res.data.data.length != 0){
+                                   self.setData({
+                                          shopName: res.data.data[0].name,
+                                          name: res.data.data[0].tuanZhangName,
+                                          mobile: res.data.data[0].mobile,
+                                          addressInfo: {
+                                                 provinceName: res.data.data[0].addressInfo.province,
+                                                 cityName: res.data.data[0].addressInfo.city,
+                                                 countyName: res.data.data[0].addressInfo.area,
+                                                 detailInfo: res.data.data[0].addressInfo.detailInfo,
+                                          },
+                                          hasAddress:true,
+                                          isModify:true,
+                                          idCardFront: res.data.data[0].idCardImgDataFront,
+                                          idCardBack: res.data.data[0].idCardImgDataBack,
+                                   })
+                            }
+                     },
+              })
+       },
+
+       chooseAddress() {
+              wx.chooseAddress({
+                     success: (res) => {
+                            this.setData({
+                                   addressInfo: res,
+                                   hasAddress: true
+                            })
+                     },
+                     fail: function (err) {
+                            console.log(err)
+                     }
+              })
+       },
+
        //添加Banner  
-       bindChooiceProduct: function() {
+       chooseFront: function() {
               var that = this;
               wx.chooseImage({
-                     count: 2, //最多可以选择的图片总数  
+                     count: 1, //最多可以选择的图片总数  
                      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有  
                      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有  
                      success: function(res) {
@@ -39,69 +92,126 @@ Page({
                                    duration: 10000
                             })
                             var uploadImgCount = 0;
-                            for (var i = 0, h = tempFilePaths.length; i < h; i++) {
-                                   wx.uploadFile({
-                                          url: util.getClientSetting().domainName + '/home/uploadfilenew',
-                                          filePath: tempFilePaths[i],
-                                          name: 'uploadfile_ant',
-                                          formData: {
-                                                 'imgIndex': i
-                                          },
-                                          header: {
-                                                 "Content-Type": "multipart/form-data"
-                                          },
-                                          success: function(res) {
-                                                 uploadImgCount++;
-                                                 var data = JSON.parse(res.data);
-                                                 //服务器返回格式: { "Catalog": "testFolder", "FileName": "1.jpg", "Url": "https://test.com/1.jpg" }  
-                                                 var productInfo = that.data.productInfo;
-                                                 if (productInfo.bannerInfo == null) {
-                                                        productInfo.bannerInfo = [];
-                                                 }
-                                                 productInfo.bannerInfo.push({
-                                                        "catalog": data.Catalog,
-                                                        "fileName": data.FileName,
-                                                        "url": data.Url
-                                                 });
-                                                 that.setData({
-                                                        productInfo: productInfo
-                                                 });
 
-                                                 //如果是最后一张,则隐藏等待中  
-                                                 if (uploadImgCount == tempFilePaths.length) {
-                                                        wx.hideToast();
-                                                 }
-                                          },
-                                          fail: function(res) {
-                                                 wx.hideToast();
-                                                 wx.showModal({
-                                                        title: '错误提示',
-                                                        content: '上传图片失败',
-                                                        showCancel: false,
-                                                        success: function(res) {}
-                                                 })
-                                          }
-                                   });
-                            }
+                            wx.uploadFile({
+                                   url: app.globalData.serverUrl + '/rest/littlecat/caobao/tuan/uploadIdCardFrontImg/' + app.globalData.openID,
+                                   filePath: tempFilePaths[0],
+                                   name: 'idCardFrontImg',
+                                   formData: {
+                                          'imgIndex': 0
+                                   },
+                                   header: {
+                                          "Content-Type": "multipart/form-data"
+                                   },
+                                   success: function(res) {
+                                          var data = JSON.parse(res.data);
+                                          console.log("uploadFront",res);
+                                          that.setData({
+                                                 idCardFront: data.data[0]
+                                          });
+                                          wx.hideToast();
+                                   },
+                                   fail: function(res) {
+                                          wx.hideToast();
+                                          wx.showModal({
+                                                 title: '错误提示',
+                                                 content: '上传图片失败',
+                                                 showCancel: false,
+                                                 success: function(res) {}
+                                          })
+                                   }
+                            });
+
+                     }
+              });
+       },
+
+       //添加Banner  
+       chooseBack: function () {
+              var that = this;
+              wx.chooseImage({
+                     count: 1, //最多可以选择的图片总数  
+                     sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有  
+                     sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有  
+                     success: function (res) {
+                            // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片  
+                            var tempFilePaths = res.tempFilePaths;
+                            //启动上传等待中...  
+                            wx.showToast({
+                                   title: '正在上传...',
+                                   icon: 'loading',
+                                   mask: true,
+                                   duration: 10000
+                            })
+                            var uploadImgCount = 0;
+
+                            wx.uploadFile({
+                                   url: app.globalData.serverUrl + '/rest/littlecat/caobao/tuan/uploadIdCardBackImg/' + app.globalData.openID,
+                                   filePath: tempFilePaths[0],
+                                   name: 'idCardBackImg',
+                                   formData: {
+                                          'imgIndex': 0
+                                   },
+                                   header: {
+                                          "Content-Type": "multipart/form-data"
+                                   },
+                                   success: function (res) {
+                                          var data = JSON.parse(res.data);
+                                          console.log("uploadBack", res);
+                                          that.setData({
+                                                 idCardBack: data.data[0]
+                                          });
+                                          wx.hideToast();
+                                   },
+                                   fail: function (res) {
+                                          wx.hideToast();
+                                          wx.showModal({
+                                                 title: '错误提示',
+                                                 content: '上传图片失败',
+                                                 showCancel: false,
+                                                 success: function (res) { }
+                                          })
+                                   }
+                            });
+
                      }
               });
        },
 
        formSubmit(e) {
+              var self = this;
+              var url = "add";
+        
+              if (self.data.isModify){
+                     url = "modify"
+              }
               const value = e.detail.value;
-              if (value.name && value.phone && value.detail) {
+              if (value.shopName && value.name && value.mobile && self.data.hasAddress) {
                      wx.request({
-                            url: app.globalData.serverUrl + "/rest/littlecat/caobao/order/create", //给函数传递服务器地址参数
+                            url: app.globalData.serverUrl + "/rest/littlecat/caobao/tuan/" + url, //给函数传递服务器地址参数
                             data: {
-                                   
+                                   id: app.globalData.openID,
+                                   tuanZhangName: value.name,
+                                   name: value.shopName,
+                                   mobile: value.mobile,
+                                   addressInfo:{
+                                          province: self.data.addressInfo.provinceName,
+                                          city: self.data.addressInfo.cityName,
+                                          area: self.data.addressInfo.countyName,
+                                          detailInfo: self.data.addressInfo.detailInfo,
+                                   }
+
                             }, //给服务器传递数据，本次请求不需要数据，可以不填
                             method: "POST",
                             header: {
                                    'content-type': 'application/json' // 默认值，返回的数据设置为json数组格式
                             },
-                            success: function (res) {
+                            success: function(res) {
                                    console.log('groupOwner', res);
                                    //pay weixin
+                                   self.setData({
+                                          isModify: true
+                                   });
                             },
                      })
               } else {
